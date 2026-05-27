499168b73dd7295efc0fbc028c2808c2af91c101
Padmaksh Paul <padmaksh09@gmail.com>
2026-05-27 09:51:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-// *********************
-// Let’s practice using Postman. Make sure your server is running with nodemon.
-// Then test the 5 different routes below with Postman. Open a separate tab for each request.
-// Check that for each route you’re getting the correct status code returned to you from your server.
-// You should not get any 404s or 500 status codes.
-// *********************
 
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
@@ -18,15 +12,15 @@ app.post("/register", (req, res) => {
   res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
+app.put("/user/padmaksh", (req, res) => {
   res.sendStatus(200);
 });
 
-app.patch("/user/angela", (req, res) => {
+app.patch("/user/padmaksh", (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete("/user/angela", (req, res) => {
+app.delete("/user/padmaksh", (req, res) => {
   //Deleting
   res.sendStatus(200);
 });
